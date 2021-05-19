ca3141d1d9ff4ba23cfc308cad3388cecc2aaa95
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-05-19 18:41:25 +0300

    fixes

--- src/main/scala/ru/ifmo/backend_2021/quill_examples/RunServer.scala
+++ src/main/scala/ru/ifmo/backend_2021/quill_examples/RunServer.scala
@@ -1,10 +1,9 @@
 package ru.ifmo.backend_2021.quill_examples
 
 import com.opentable.db.postgres.embedded.EmbeddedPostgres
+import scala.io.StdIn.readLine
 
 object RunServer extends App {
   val server = EmbeddedPostgres.builder().setPort(5432).start()
-  while (true) {
-    Thread.sleep(10000)
-  }
+  readLine()
 }
